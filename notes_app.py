3d4ead05c61bffc8ff15b04ab8d697b2a9a2b15c
Amal Ali Meslmany <amal.alimeslmany@gmail.com>
2026-07-04 20:45:38 +0300

    notesapp

--- notes_app.py
+++ notes_app.py
@@ -2,27 +2,40 @@ while True:
 
     print("\n===== NOTES APP =====")
     print("1. Add Note")
-    print("2. Exit")
+    print("2. View Notes")
+    print("3. Delete All Notes")
+    print("4. Exit")
 
     choice = input("Choose: ")
 
     if choice == "1":
-
         note = input("Enter your note: ")
 
         file = open("notes.txt", "a")
-
         file.write(note + "\n")
-
         file.close()
 
         print("Note Added Successfully!")
 
     elif choice == "2":
+        file = open("notes.txt", "r")
+        notes = file.read()
+        file.close()
 
-        print("Goodbye!")
-        break
+        print("\n===== YOUR NOTES =====")
+        print(notes)
 
-    else:
+    elif choice == "3":
 
+         file = open("notes.txt", "w")
+         file.close()
+
+         print("All Notes Deleted!")
+
+    elif choice == "4":
+
+         print("Goodbye!")
+         break
+
+else:
         print("Invalid Choice!")
